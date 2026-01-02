846774f8cc012138aa9aeb8d91deadf9ae091ff3
Abdelrahman <abdelrhman2607@gmail.com>
2026-01-02 16:15:55 -0700

    generate delivery option HTML

--- JS-amazon-project/data/cart.js
+++ JS-amazon-project/data/cart.js
@@ -1,15 +1,16 @@
 export let cart = JSON.parse(localStorage.getItem("cart"));
 
-console.log(JSON.parse(localStorage.getItem("cart")));
 if (!cart){
     cart = [
     {
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2
+        quantity: 2,
+        deliveryOptionId: '1'
     },
     {
         productId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
-        quantity: 1
+        quantity: 1,
+        deliveryOptionId: '1'
     }
     ];
 }
@@ -34,7 +35,8 @@ export function addToCart(productId){
         cart.push(
             {
             productId: productId,
-            quantity: 1
+            quantity: 1,
+            deliveryOptionId: '1'
             }
         );
     }
