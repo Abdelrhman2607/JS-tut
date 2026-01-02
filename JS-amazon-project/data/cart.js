4767d10fe327d4f80ba312db041ecf32ae1e82f8
Abdelrahman <abdelrhman2607@gmail.com>
2026-01-02 02:32:27 -0700

    move addToCart to cart.js

--- JS-amazon-project/data/cart.js
+++ JS-amazon-project/data/cart.js
@@ -1,1 +1,23 @@
 export const cart = [];
+
+export function addToCart(productId){
+    let matchingcartItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId){
+            matchingcartItem = cartItem;
+        }
+    });
+
+    if (matchingItem){
+        matchingItem.quantity++;
+    }
+    else{
+        cart.push(
+            {
+            productId: productId,
+            quantity: 1
+            }
+        );
+    }
+}
