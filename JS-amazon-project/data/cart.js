9ad864929aa7bde36fb35e0ed89443ede9431b48
Abdelrahman <abdelrhman2607@gmail.com>
2026-01-02 03:26:26 -0700

    cart localStorage

--- JS-amazon-project/data/cart.js
+++ JS-amazon-project/data/cart.js
@@ -1,4 +1,8 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+console.log(JSON.parse(localStorage.getItem("cart")));
+if (!cart){
+    cart = [
     {
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
         quantity: 2
@@ -7,14 +11,19 @@ export let cart = [
         productId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
         quantity: 1
     }
-];
+    ];
+}
+
+function saveToStorage(){
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId){
-    let matchingcartItem;
+    let matchingItem;
 
     cart.forEach((cartItem) => {
         if (productId === cartItem.productId){
-            matchingcartItem = cartItem;
+            matchingItem = cartItem;
         }
     });
 
@@ -29,6 +38,7 @@ export function addToCart(productId){
             }
         );
     }
+    saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -41,4 +51,5 @@ export function removeFromCart(productId){
     });
 
     cart = newCart;
+    saveToStorage();
 }
