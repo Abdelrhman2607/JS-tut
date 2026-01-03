e4c152f930851f48f33a3e9b1714b5a6f2475d50
Abdelrahman <abdelrhman2607@gmail.com>
2026-01-02 21:57:38 -0700

    test for addToCart

--- JS-amazon-project/data/cart.js
+++ JS-amazon-project/data/cart.js
@@ -1,19 +1,6 @@
-export let cart = JSON.parse(localStorage.getItem("cart"));
+export let cart;
 
-if (!cart){
-    cart = [
-    {
-        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2,
-        deliveryOptionId: '1'
-    },
-    {
-        productId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
-        quantity: 1,
-        deliveryOptionId: '1'
-    }
-    ];
-}
+loadFromStorage();
 
 function saveToStorage(){
     localStorage.setItem("cart", JSON.stringify(cart));
@@ -67,4 +54,22 @@ export function updateDeliveryOption(productId, deliveryOptionId){
 
     matchingItem.deliveryOptionId = deliveryOptionId;
     saveToStorage();
+}
+
+export function loadFromStorage(){
+cart = JSON.parse(localStorage.getItem("cart"));
+if (!cart){
+    cart = [
+    {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: '1'
+    },
+    {
+        productId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+        quantity: 1,
+        deliveryOptionId: '1'
+    }
+    ];
+}
 }
