f44ae76a72ad80cc250e0039de63c5ebda283c06
Abdelrahman <abdelrhman2607@gmail.com>
2026-01-04 01:16:42 -0700

    checkout promises

--- JS-amazon-project/data/cart.js
+++ JS-amazon-project/data/cart.js
@@ -73,3 +73,14 @@ if (!cart){
     ];
 }
 }
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
--- JS-amazon-project/data/products.js
+++ JS-amazon-project/data/products.js
@@ -53,6 +53,7 @@ export function loadProducts(fun) {
 
       return new Product(productDetails);
     });
+    console.log('load products');
     fun();
   });
 
