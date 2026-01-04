f7a223754fac0f5d025eccc744189f000473d4f5
Abdelrahman <abdelrhman2607@gmail.com>
2026-01-04 02:52:15 -0700

    order backend

--- JS-amazon-project/scripts/checkout/paymentSummary.js
+++ JS-amazon-project/scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import {getDeliveryOption} from '../../data/deliveryOptions.js';
 import formatCurrency from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary(){
 
@@ -50,10 +51,32 @@ export function renderPaymentSummary(){
             <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="js-place-order-button place-order-button button-primary">
             Place your order
           </button>
     `;
     
     document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+    document.querySelector('.js-place-order-button').addEventListener("click", async () => {
+      try{
+          const response = await fetch('https://supersimplebackend.dev/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: {
+            cart: JSON.stringify(cart)
+          }
+        });
+
+        const order = await response.json();
+        addOrder(order);
+      }
+      catch(error){
+        console.log(":C");
+      }
+
+      window.location.href = "orders.html";
+    });
 }
