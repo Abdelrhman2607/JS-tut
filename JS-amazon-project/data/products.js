7dd29da8152336671cfa82c87c5d23d534a6ce7c
Abdelrahman <abdelrhman2607@gmail.com>
2026-01-03 22:49:05 -0700

    load products from backend

--- JS-amazon-project/data/products.js
+++ JS-amazon-project/data/products.js
@@ -42,6 +42,24 @@ class Clothing extends Product{
   }
 }
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing')
+        return new Clothing(productDetails);
+
+      return new Product(productDetails);
+    });
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -701,12 +719,8 @@ export const products = [
       "mens"
     ]
   }
-].map((productDetails) => {
-  if (productDetails.type === 'clothing')
-    return new Clothing(productDetails);
-
-  return new Product(productDetails);
-});
+]
+*/
 
 export function getProduct(productId){
   let matchingProduct;
